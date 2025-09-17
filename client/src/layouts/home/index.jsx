e1cd18766a2cf8b36eb43b6e5f6489d55cfbfdb3
Big Dev <badejoemmanuel805@gmail.com>
2025-09-17 21:27:06 +1300

    Revert "Complete mobile responsiveness restructure"
    This reverts commit 097e00313846971b5951ab42f7ea1ef33dba44be.

--- client/src/layouts/home/index.jsx
+++ client/src/layouts/home/index.jsx
@@ -5,15 +5,15 @@ import { Outlet } from "react-router-dom"
 const HomeLayout = () => {
       return (
 
-            <div className='w-full flex flex-col overflow-hidden bg-[#F3F3F4] h-[100dvh]'>
+            <div className='w-full  flex flex-col overflow-hidden bg-[#F3F3F4] h-[100dvh]'>
                   <div className='min-h-8 h-8 w-full'>
                   </div>
                   <div className='w-full flex flex-1'>
                         {/* Left nav bar */}
-                        <div className='w-[4.5rem] max-md:w-[3.5rem] max-sm:w-[3rem]'>
+                        <div className='w-[4.5rem]'>
                               <Navigation />
                         </div>
-                        <div className='border overflow-hidden rounded-tl-lg border-border flex-1 max-md:rounded-none'>
+                        <div className='border overflow-hidden rounded-tl-lg  border-border flex-1'>
                               <Outlet />
                         </div>
                   </div>
